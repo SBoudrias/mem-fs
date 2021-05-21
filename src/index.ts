5fc0f7884872ed270abed0695bbc7fdfaf9108b5
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-05-21 19:38:26 -0400

    Finish TypeScript
    - Convert Store class to ES6 format so tsc can generate typings
    - Enable strict mode in tsconfig
    - Add exclude to tsconfig
    - Add tsc step to prepublish
    - Indicate `main` and `types` in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,70 @@
-import events from 'events';
+import { EventEmitter } from 'events';
 import path from 'path';
-import util from 'util';
 import vinylFile from 'vinyl-file';
 import File from 'vinyl';
 import { PassThrough } from 'stream';
 
-export const create = function () {
-  var store = {};
+function createFile(filepath: string) {
+  return new File({
+    cwd: process.cwd(),
+    base: process.cwd(),
+    path: filepath,
+    contents: null
+  });
+}
 
-  function createFile(filepath) {
-    return new File({
-      cwd: process.cwd(),
-      base: process.cwd(),
-      path: filepath,
-      contents: null
-    });
-  }
+export class Store extends EventEmitter {
+  private store: Record<string, File> = {};
 
-  function load(filepath) {
-    var file;
+  private load(filepath: string): File {
+    let file: File;
     try {
       file = vinylFile.readSync(filepath);
     } catch (err) {
       file = createFile(filepath);
     }
-    store[filepath] = file;
+    this.store[filepath] = file;
     return file;
   }
 
-  var Store = function () {
-    events.EventEmitter.apply(this, arguments);
-  };
-  util.inherits(Store, events.EventEmitter);
-
-  Store.prototype.get = function (filepath) {
+  get(filepath: string): File {
     filepath = path.resolve(filepath);
-    return store[filepath] || load(filepath);
-  };
+    return this.store[filepath] || this.load(filepath);
+  }
 
-  Store.prototype.existsInMemory = function (filepath) {
+  existsInMemory(filepath: string): boolean {
     filepath = path.resolve(filepath);
-    return !!store[filepath];
-  };
+    return !!this.store[filepath];
+  }
 
-  Store.prototype.add = function (file) {
-    store[file.path] = file;
+  add(file: File): this {
+    this.store[file.path] = file;
     this.emit('change', file.path);
     return this;
-  };
+  }
 
-  Store.prototype.each = function (onEach) {
-    Object.keys(store).forEach(function (key, index) {
-      onEach(store[key], index);
+  each(onEach: (file: File, index: number) => void): this {
+    Object.keys(this.store).forEach((key, index) => {
+      onEach(this.store[key], index);
     });
     return this;
-  };
+  }
 
-  Store.prototype.all = function () {
-    return Object.values(store);
-  };
+  all(): File[] {
+    return Object.values(this.store);
+  }
 
-  Store.prototype.stream = function () {
-    const stream = new PassThrough({objectMode: true, autoDestroy: true});
-    setImmediate(function () {
-      this.each((file) => stream.write(file));
+  stream(): PassThrough {
+    const stream = new PassThrough({ objectMode: true, autoDestroy: true });
+    setImmediate(() => {
+      this.each((file: File) => stream.write(file));
       stream.end();
-    }.bind(this));
-    return stream;
-  };
+    });
 
+    return stream;
+  }
+}
+
+export function create(): Store {
   return new Store();
 };
